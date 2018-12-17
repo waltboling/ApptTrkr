cf95609383c577b70b28b0550b8d78b452613a25
Walt Boling <bolingjw@gmail.com>
2018-12-17 13:04:00 -0600

    Initial Commit

---
+++ ApptTrkr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApptTrkr
+//
+//  Created by Jon Boling on 12/17/18.
+//  Copyright © 2018 Walt Boling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
